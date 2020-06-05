d2f9906c21a431abf154264c58fd8a4d8c9ec84c
Alexander Gagarin <sacred@xaker.ru>
2020-06-05 21:33:55 +0500

    method Character

--- 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
+++ 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
@@ -6,7 +6,7 @@ public class Main
         Container container = new Container();
         container.count += 7843;
         Container container2 = new Container();
-        container2.count += 1546;
+        container2.count += 1541;
         System.out.println("Число: " + container.count);
         System.out.println("Сумма цифр в числе: " + sumDigits(container.count));
         System.out.println("Число: " + container2.count);
@@ -16,10 +16,10 @@ public class Main
     public static Integer sumDigits(Integer number)
     {
         String sNum1 = String.valueOf(number);
-        int num1 = Integer.parseInt(sNum1.substring(0,1));
-        int num2 = Integer.parseInt(sNum1.substring(1,2));
-        int num3 = Integer.parseInt(sNum1.substring(2,3));
-        int num4 = Integer.parseInt(sNum1.substring(3,4));
+        int num1 = Integer.parseInt(Character.toString(sNum1.charAt(0)));
+        int num2 = Integer.parseInt(Character.toString(sNum1.charAt(1)));
+        int num3 = Integer.parseInt(Character.toString(sNum1.charAt(2)));
+        int num4 = Integer.parseInt(Character.toString(sNum1.charAt(3)));
         int sum = num1 + num2 + num3 + num4;
         return sum;
     }
