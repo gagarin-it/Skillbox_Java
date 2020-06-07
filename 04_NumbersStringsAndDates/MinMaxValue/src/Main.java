32039c2dc9efb99a6dffa7bf4073dd0fc8e95b57
Alexander Gagarin <sacred@xaker.ru>
2020-06-08 00:57:35 +0500

    change min_value variables doble and float

--- 04_NumbersStringsAndDates/MinMaxValue/src/Main.java
+++ 04_NumbersStringsAndDates/MinMaxValue/src/Main.java
@@ -10,9 +10,10 @@ public class Main
     System.out.println(Integer.MAX_VALUE);
     System.out.println(Long.MIN_VALUE);
     System.out.println(Long.MAX_VALUE);
-    System.out.println(Double.MIN_VALUE);
+    System.out.println(-Double.MAX_VALUE);
     System.out.println(Double.MAX_VALUE);
-    System.out.println(Float.MIN_VALUE);
+    System.out.println(-Float.MAX_VALUE);
     System.out.println(Float.MAX_VALUE);
+    
   }
 }
