25ced90304370e279cd580801ad8371c429e3bb9
Alexander Gagarin <sacred@xaker.ru>
2020-07-18 20:26:00 +0500

    homework 4.3/2

--- 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
+++ 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
@@ -3,16 +3,17 @@ public class Loader
 {
     public static void main(String[] args)
     {
-        for(char i = 'A'; i <= 'Z';i++)
-        {
-            System.out.println(i + " - " + ((int)i));
-        }
-        for(char i = 'a'; i <= 'z';i++)
-        {
-            System.out.println(i + " - " + ((int)i));
-        }
+//        for(char i = 'A'; i <= 'Z';i++)
+//        {
+//            System.out.println(i + " - " + ((int)i));
+//        }
+//        for(char i = 'a'; i <= 'z';i++)
+//        {
+//            System.out.println(i + " - " + ((int)i));
+//        }
 
-//        String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-//        System.out.println(text);
+        String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+        System.out.println(Integer.parseInt(text.substring(15,19)) + Integer.parseInt(text.substring(35,39)) + Integer.parseInt(text.substring(56,61)));
+
     }
 }
