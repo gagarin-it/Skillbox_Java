2040cf962c545b38e05239ea86f70aa1a10aa8ae
Alexander Gagarin <sacred@xaker.ru>
2020-07-18 22:07:19 +0500

    change homework 4.4/2

--- 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
+++ 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
@@ -13,7 +13,6 @@ public class Loader
 //        }
 
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-        System.out.println(Integer.parseInt(text.substring(15,19)) + Integer.parseInt(text.substring(35,39)) + Integer.parseInt(text.substring(56,61)));
-
+        System.out.println(Integer.parseInt(text.substring(15,19)) + Integer.parseInt(text.substring(56,61)));
     }
 }
