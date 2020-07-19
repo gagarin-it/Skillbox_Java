afbccaba118c503d694014120e5e97a891187a89
Alexander Gagarin <sacred@xaker.ru>
2020-07-19 12:09:25 +0500

    add homework 4.4*

--- 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
+++ 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
@@ -6,6 +6,10 @@ import javax.sound.midi.Soundbank;
 public class Loader
 {
     public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String textIn = reader.readLine();
+        System.out.println(searchAndReplaceDiamonds(textIn,"***"));
+
 //        for(char i = 'A'; i <= 'Z';i++)
 //        {
 //            System.out.println(i + " - " + ((int)i));
@@ -18,32 +22,40 @@ public class Loader
 //        String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
 //        System.out.println(Integer.parseInt(text.substring(15,19)) + Integer.parseInt(text.substring(56,61)));
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Введите ФИО в формате: Фамилия Имя Отчество");
-        String textIn = reader.readLine();
-        String lastName = "", name = "", patronymic = "";
-        int spaceIndex1 = textIn.indexOf(' ',0);
-        int spaceIndex2 = textIn.indexOf(' ', spaceIndex1 + 1);
-        int spaceIndex3 = textIn.indexOf(' ', spaceIndex2 + 1);
-        if (spaceIndex1 == -1)
-        {
-            lastName = textIn.substring(0,textIn.length());
-        }
-        else {
-            lastName = textIn.substring(0, spaceIndex1);
-            if (spaceIndex2 == -1) {
-                name = textIn.substring(spaceIndex1, textIn.length());
-            } else
-            {
-                name = textIn.substring(spaceIndex1, spaceIndex2);
-            if (spaceIndex3 == -1) {
-                patronymic = textIn.substring(spaceIndex2, textIn.length());
-            } else
-                patronymic = textIn.substring(spaceIndex2, spaceIndex3);
-            }
-        }
-
-        System.out.println("Фамилия: " + lastName + "\nИмя: " + name + "\nОтчество: " + patronymic);
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        System.out.println("Введите ФИО в формате: Фамилия Имя Отчество");
+//        String textIn = reader.readLine();
+//        String lastName = "", name = "", patronymic = "";
+//        int spaceIndex1 = textIn.indexOf(' ',0);
+//        int spaceIndex2 = textIn.indexOf(' ', spaceIndex1 + 1);
+//        int spaceIndex3 = textIn.indexOf(' ', spaceIndex2 + 1);
+//        if (spaceIndex1 == -1)
+//        {
+//            lastName = textIn.substring(0,textIn.length());
+//        }
+//        else {
+//            lastName = textIn.substring(0, spaceIndex1);
+//            if (spaceIndex2 == -1) {
+//                name = textIn.substring(spaceIndex1, textIn.length());
+//            } else
+//            {
+//                name = textIn.substring(spaceIndex1, spaceIndex2);
+//            if (spaceIndex3 == -1) {
+//                patronymic = textIn.substring(spaceIndex2, textIn.length());
+//            } else
+//                patronymic = textIn.substring(spaceIndex2, spaceIndex3);
+//            }
+//        }
+//
+//        System.out.println("Фамилия: " + lastName + "\nИмя: " + name + "\nОтчество: " + patronymic);
 
     }
+    public static String searchAndReplaceDiamonds(String text, String placeholder)
+    {
+        int index1 = text.indexOf('<');
+        int index2 = text.lastIndexOf('>');
+        text = text.substring(0,index1) + placeholder + text.substring(index2 + 1);
+        return text;
+    }
+
 }
