8a5a871bdd56e404acd2f4ed94251448d9d6bb2a
Alexander Gagarin <sacred@xaker.ru>
2020-08-02 14:40:54 +0500

    add homework 4.6 - day of week on birthday

---
+++ 04_NumbersStringsAndDates/StringExperiments/src/DayOfWeekOnBirthday.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class DayOfWeekOnBirthday {
+
+  public static void main(String[] args) {
+    Calendar calendar = Calendar.getInstance();
+    System.out.println(calendar.getTime());
+
+  }
+}
--- 04_NumbersStringsAndDates/StringExperiments/src/SearchDiamonds.java
+++ 04_NumbersStringsAndDates/StringExperiments/src/SearchDiamonds.java
@@ -7,10 +7,10 @@ public class SearchDiamonds {
   public static void main(String[] args) throws IOException {
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     String textIn = reader.readLine();
-    System.out.println(searchAndReplaceDiamonds(textIn,"***"));
+    System.out.println(searchAndReplaceDiamonds(textIn, "***"));
   }
-  public static String searchAndReplaceDiamonds(String text, String placeholder)
-  {
+
+  public static String searchAndReplaceDiamonds(String text, String placeholder) {
     text = text.replaceAll("<\\w+>", placeholder);
     return text;
   }
