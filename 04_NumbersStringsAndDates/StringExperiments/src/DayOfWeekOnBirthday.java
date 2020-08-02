5c5053d8f232499cb083d16e1ee82ee1e8a46ff3
Alexander Gagarin <sacred@xaker.ru>
2020-08-02 16:59:59 +0500

    homework 4.6 - day of week on birthday

--- 04_NumbersStringsAndDates/StringExperiments/src/DayOfWeekOnBirthday.java
+++ 04_NumbersStringsAndDates/StringExperiments/src/DayOfWeekOnBirthday.java
@@ -1,10 +1,49 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class DayOfWeekOnBirthday {
 
-  public static void main(String[] args) {
-    Calendar calendar = Calendar.getInstance();
-    System.out.println(calendar.getTime());
+  public static void main(String[] args) throws IOException {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Введите день Вашего рождения: ");
+    int day = Integer.parseInt(reader.readLine());
+    System.out.println("Введите месяц Вашего рождения: ");
+    int month = Integer.parseInt(reader.readLine());
+    System.out.println("Введите год Вашего рождения: ");
+    int year = Integer.parseInt(reader.readLine());
 
+    Calendar dateNow = new GregorianCalendar();
+    Calendar dateBorn = new GregorianCalendar(year, month - 1, day);
+    DateFormat dateFormat = new SimpleDateFormat(" - dd.MM.YYYY - EEEE");
+    int differenceYear = dateNow.get(Calendar.YEAR) - dateBorn.get(Calendar.YEAR);
+
+    if (dateBorn.get(Calendar.YEAR) > dateNow.get(Calendar.YEAR)) {
+      System.err.println("Вы ввели год, превышающий текущий!");
+    }
+    for (int i = 0; i <= differenceYear; i++) {
+      if (dateBorn.get(Calendar.YEAR) == dateNow.get(Calendar.YEAR)
+          && dateBorn.get(Calendar.MONTH) > dateNow.get(Calendar.MONTH)) {
+        System.err.println("Вы ввели месяц, превышающий текущий!");
+        break;
+      }
+      if (dateBorn.get(Calendar.YEAR) == dateNow.get(Calendar.YEAR)
+          && dateBorn.get(Calendar.DATE) > dateNow.get(Calendar.DATE)) {
+        System.err.println("Вы ввели число, превышающее текущее!");
+        break;
+      }
+      System.out.println(i + dateFormat.format(dateBorn.getTime()));
+      dateBorn.add(Calendar.YEAR, 1);
+      if ((dateBorn.get(Calendar.YEAR) == dateNow.get(Calendar.YEAR)
+          && dateBorn.get(Calendar.DATE) > dateNow.get(Calendar.DATE)) || (
+          dateBorn.get(Calendar.YEAR) == dateNow.get(Calendar.YEAR)
+              && dateBorn.get(Calendar.MONTH) > dateNow.get(Calendar.MONTH))) {
+        break;
+      }
+    }
   }
 }
