af9ede3d88c00a948bf023a48cf7ef53a693584e
Alexander Gagarin <sacred@xaker.ru>
2020-08-02 14:14:49 +0500

    add homework SearchAndReplaceDiamods - 4.5/*

--- 04_NumbersStringsAndDates/StringExperiments/src/SearchDiamonds.java
+++ 04_NumbersStringsAndDates/StringExperiments/src/SearchDiamonds.java
@@ -1,5 +1,18 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class SeachDiamonds {
+public class SearchDiamonds {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    String textIn = reader.readLine();
+    System.out.println(searchAndReplaceDiamonds(textIn,"***"));
+  }
+  public static String searchAndReplaceDiamonds(String text, String placeholder)
+  {
+    text = text.replaceAll("<\\w+>", placeholder);
+    return text;
+  }
 
 }
