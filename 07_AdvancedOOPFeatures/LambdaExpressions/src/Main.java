f43fe9306a0d6ec0f515a109c9a0fea4d47047e1
Alexander Gagarin <sacred@xaker.ru>
2020-10-24 17:32:37 +0500

    add Employee with max salary and workstart in 2017 year

--- 07_AdvancedOOPFeatures/LambdaExpressions/src/Main.java
+++ 07_AdvancedOOPFeatures/LambdaExpressions/src/Main.java
@@ -2,48 +2,56 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Calendar;
+import java.util.Comparator;
+import java.util.GregorianCalendar;
 import java.util.List;
 
-public class Main
-{
-    private static String staffFile = "D:/Skillbox/java_basics/07_AdvancedOOPFeatures/LambdaExpressions/data/staff.txt";
-    private static String dateFormat = "dd.MM.yyyy";
+public class Main {
 
-    public static void main(String[] args)
-    {
-        ArrayList<Employee> staff = loadStaffFromFile();
-        Collections.sort(staff, (o1, o2) -> o1.getName().compareTo(o2.getName()));
-        Collections.sort(staff, (o1, o2) -> o1.getSalary().compareTo(o2.getSalary()));
+  private static String staffFile = "D:/Skillbox/java_basics/07_AdvancedOOPFeatures/LambdaExpressions/data/staff.txt";
+  private static String dateFormat = "dd.MM.yyyy";
 
-        for(Employee employee : staff){
-            System.out.println(employee);
+  public static void main(String[] args) {
+    ArrayList<Employee> staff = loadStaffFromFile();
+    staff.sort(Comparator.comparing(Employee::getName));
+    staff.sort(Comparator.comparing(Employee::getSalary));
+//        staff.forEach(System.out::println);
+//        System.out.println("_________________________");
+//        staff.forEach(e -> e.setSalary(e.getSalary() + 10000));
+//        staff.forEach(System.out::println);
+//        System.out.println("_________________________");
+//        staff.stream().filter(e -> e.getSalary() >= 99000).forEach(System.out::println);
+//        staff.stream().min(Comparator.comparing(Employee::getSalary)).ifPresent(System.out::println);
+//        System.out.println("_________________________");
+    staff.stream().filter(
+        employee -> employee.getWorkStart().after(new GregorianCalendar(2017, Calendar.JANUARY, 1).getTime())
+            && employee.getWorkStart().before(new GregorianCalendar(2018, Calendar.JANUARY, 1).getTime()))
+        .max(Comparator.comparing(Employee::getSalary)).ifPresent(
+        System.out::println);
+
+
+  }
+
+  private static ArrayList<Employee> loadStaffFromFile() {
+    ArrayList<Employee> staff = new ArrayList<>();
+    try {
+      List<String> lines = Files.readAllLines(Paths.get(staffFile));
+      for (String line : lines) {
+        String[] fragments = line.split("\t");
+        if (fragments.length != 3) {
+          System.out.println("Wrong line: " + line);
+          continue;
         }
+        staff.add(new Employee(
+            fragments[0],
+            Integer.parseInt(fragments[1]),
+            (new SimpleDateFormat(dateFormat)).parse(fragments[2])
+        ));
+      }
+    } catch (Exception ex) {
+      ex.printStackTrace();
     }
-
-    private static ArrayList<Employee> loadStaffFromFile()
-    {
-        ArrayList<Employee> staff = new ArrayList<>();
-        try
-        {
-            List<String> lines = Files.readAllLines(Paths.get(staffFile));
-            for(String line : lines)
-            {
-                String[] fragments = line.split("\t");
-                if(fragments.length != 3) {
-                    System.out.println("Wrong line: " + line);
-                    continue;
-                }
-                staff.add(new Employee(
-                    fragments[0],
-                    Integer.parseInt(fragments[1]),
-                    (new SimpleDateFormat(dateFormat)).parse(fragments[2])
-                ));
-            }
-        }
-        catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        return staff;
-    }
+    return staff;
+  }
 }
