83038d934bef8f99c8e0d57b405a9e61a75c0e95
Alexander Gagarin <sacred@xaker.ru>
2020-11-04 13:21:21 +0500

    add catch exception

--- 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
+++ 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
@@ -1,4 +1,6 @@
 import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CustomerStorage
 {
@@ -9,9 +11,26 @@ public class CustomerStorage
         storage = new HashMap<>();
     }
 
-    public void addCustomer(String data)
-    {
+    public void addCustomer(String data) {
         String[] components = data.split("\\s+");
+        if (components.length != 4) {
+            throw new IllegalArgumentException(
+                "Не верный формат записи. Введите согласно формата: \n"
+                    + "add Василий Петров vasily.petrov@gmail.com +79215637722");
+        }
+        Pattern patternNumber = Pattern
+            .compile("^((8|\\+7)[\\- ]?)?(\\(?\\d{3}\\)?[\\- ]?)?[\\d\\- ]{10}$");
+        Matcher matcherNumber = patternNumber.matcher(components[3]);
+        if (!matcherNumber.find()) {
+            throw new NumberFormatException(
+                "Не верный формат телефонного номера. Введите согласно формата: +79215637722");
+        }
+        Pattern patternEmail = Pattern.compile("(?i)([A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4})");
+        Matcher matcherEmail = patternEmail.matcher(components[2]);
+        if (!matcherEmail.find()) {
+            throw new IllegalArgumentException(
+                "Не верный формат адреса электронной почты. Введите согласно формата: vasily.petrov@gmail.com");
+        }
         String name = components[0] + " " + components[1];
         storage.put(name, new Customer(name, components[3], components[2]));
     }
@@ -21,8 +40,11 @@ public class CustomerStorage
         storage.values().forEach(System.out::println);
     }
 
-    public void removeCustomer(String name)
-    {
+    public void removeCustomer(String name) {
+        if (name.split("\\s+").length != 2) {
+            throw new IllegalArgumentException(
+                "Не верный формат записи. Введите согласно формата: remove Василий Петров");
+        }
         storage.remove(name);
     }
 
--- 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/Main.java
+++ 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/Main.java
@@ -16,26 +16,25 @@ public class Main
         CustomerStorage executor = new CustomerStorage();
         for(;;)
         {
-            String command = scanner.nextLine();
-            String[] tokens = command.split("\\s+", 2);
-            if(tokens[0].equals("add")) {
-                executor.addCustomer(tokens[1]);
+            try {
+                String command = scanner.nextLine();
+                String[] tokens = command.split("\\s+", 2);
+                if (tokens[0].equals("add")) {
+                    executor.addCustomer(tokens[1]);
+                } else if (tokens[0].equals("list")) {
+                    executor.listCustomers();
+                } else if (tokens[0].equals("remove")) {
+                    executor.removeCustomer(tokens[1]);
+                } else if (tokens[0].equals("count")) {
+                    System.out.println("There are " + executor.getCount() + " customers");
+                } else if (tokens[0].equals("help")) {
+                    System.out.println(helpText);
+                } else {
+                    System.out.println(commandError);
+                }
             }
-            else if(tokens[0].equals("list")) {
-                executor.listCustomers();
-            }
-            else if(tokens[0].equals("remove"))
-            {
-                executor.removeCustomer(tokens[1]);
-            }
-            else if(tokens[0].equals("count")) {
-                System.out.println("There are " + executor.getCount() + " customers");
-            }
-            else if(tokens[0].equals("help")) {
-                System.out.println(helpText);
-            }
-            else {
-                System.out.println(commandError);
+            catch (Exception ex){
+                System.err.println(ex.getMessage());
             }
         }
     }
