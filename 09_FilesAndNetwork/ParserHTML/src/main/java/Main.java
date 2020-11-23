9e72ca589fbbf66db93bd2fbde0007578b650894
Alexander Gagarin <sacred@xaker.ru>
2020-11-23 14:29:55 +0500

    change method downloadImage in project ParserHTML, use dependencty common-io

--- 09_FilesAndNetwork/ParserHTML/src/main/java/Main.java
+++ 09_FilesAndNetwork/ParserHTML/src/main/java/Main.java
@@ -1,11 +1,7 @@
-import java.io.FileOutputStream;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import org.apache.commons.io.FileUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -27,24 +23,9 @@ public class Main {
 
   private static void downloadImage(String strImageURL) {
     try {
-      Path pathFolder = Paths.get(OUTPUT_FOLDER_SAVED_IMAGE);
-      if(!Files.exists(pathFolder)){
-        Files.createDirectory(pathFolder);
-      }
       String strImageName = strImageURL.substring(strImageURL.lastIndexOf("/") + 1);
-
       URL urlImage = new URL(strImageURL);
-      InputStream in = urlImage.openStream();
-
-      byte[] buffer = new byte[4096];
-      int n = -1;
-
-      OutputStream os =
-          new FileOutputStream(OUTPUT_FOLDER_SAVED_IMAGE + "/" + strImageName);
-      while ((n = in.read(buffer)) != -1) {
-        os.write(buffer, 0, n);
-      }
-      os.close();
+      FileUtils.copyURLToFile(urlImage,new File(OUTPUT_FOLDER_SAVED_IMAGE,strImageName));
       System.out.println("Saved: " + strImageName + ", from: " + strImageURL);
 
     } catch (IOException e) {
