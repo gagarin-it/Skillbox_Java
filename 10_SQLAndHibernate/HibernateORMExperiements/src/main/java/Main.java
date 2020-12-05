278356aa8f3933a1a7d882c12effc7a6326782e6
Alexander Gagarin <sacred@xaker.ru>
2020-12-05 17:57:23 +0500

    connection Hibernate & print info Courses

---
+++ 10_SQLAndHibernate/HibernateORMExperiements/src/main/java/Main.java
@@ -0,0 +1,23 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class Main {
+
+  public static void main(String[] args) {
+    StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+        .configure("hibernate.cfg.xml").build();
+    Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
+    SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+
+    Session session = sessionFactory.openSession();
+    for(int i = 1; i < 47;i++){
+      Course course = session.get(Course.class,i);
+      System.out.printf("%-35s: %d человек%n", course.getName(),course.getStudentsCount());
+    }
+    sessionFactory.close();
+  }
+}
--- 10_SQLAndHibernate/MySQL&JDBCExperiments/src/main/java/Main.java
+++ 10_SQLAndHibernate/MySQL&JDBCExperiments/src/main/java/Main.java
@@ -10,16 +10,13 @@ public class Main {
   private static final String PASSWORD_MYSQL = "test";
 
   public static void main(String[] args) {
-    try {
-      Connection connection = DriverManager.getConnection(URL_MYSQL, USER_MYSQL, PASSWORD_MYSQL);
+    try (Connection connection = DriverManager.getConnection(URL_MYSQL, USER_MYSQL, PASSWORD_MYSQL)){
       Statement statement = connection.createStatement();
-      ResultSet resultSet = statement.executeQuery(
-          "SELECT pl.course_name, (count(month(pl.subscription_date))/max(month(pl.subscription_date))) as "
-              + "average_of_count_buy FROM PurchaseList pl group by pl.course_name order by pl.course_name");
+      ResultSet resultSet = statement.executeQuery("SELECT pl.course_name, (count(month(pl.subscription_date))/"
+          + "max(month(pl.subscription_date))) as average_of_count_buy FROM PurchaseList pl "
+          + "group by pl.course_name order by pl.course_name");
       int courseId = 0;
-      System.out
-          .printf("     %-35s%-16s%n", "Наименование курса",
-              "Среднее количество покупок");
+      System.out.printf("     %-35s%-16s%n", "Наименование курса", "Среднее количество покупок");
       while (resultSet.next()) {
         courseId++;
         String courseName = resultSet.getString(1);
@@ -29,7 +26,6 @@ public class Main {
       }
       resultSet.close();
       statement.close();
-      connection.close();
     } catch (Exception e) {
       e.printStackTrace();
     }
