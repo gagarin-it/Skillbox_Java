b24e02d27ba53d5149e3529c84da641cf39407e5
Alexander Gagarin <sacred@xaker.ru>
2021-01-06 23:37:34 +0500

    added library Imgscalr

--- 11_Multithreading/ImageResizer/src/main/java/ImageResizer.java
+++ 11_Multithreading/ImageResizer/src/main/java/ImageResizer.java
@@ -1,6 +1,8 @@
 import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageOp;
 import java.io.File;
 import javax.imageio.ImageIO;
+import org.imgscalr.Scalr;
 
 public class ImageResizer implements Runnable {
 
@@ -30,20 +32,7 @@ public class ImageResizer implements Runnable {
         int newHeight = (int) Math.round(
             image.getHeight() / (image.getWidth() / (double) newWidth)
         );
-        BufferedImage newImage = new BufferedImage(
-            newWidth, newHeight, BufferedImage.TYPE_INT_RGB
-        );
-
-        int widthStep = image.getWidth() / newWidth;
-        int heightStep = image.getHeight() / newHeight;
-
-        for (int x = 0; x < newWidth; x++)
-        {
-          for (int y = 0; y < newHeight; y++) {
-            int rgb = image.getRGB(x * widthStep, y * heightStep);
-            newImage.setRGB(x, y, rgb);
-          }
-        }
+        BufferedImage newImage = Scalr.resize(image, newWidth, newHeight, Scalr.OP_ANTIALIAS);
 
         File newFile = new File(dstFolder + "/" + file.getName());
         ImageIO.write(newImage, "jpg", newFile);
