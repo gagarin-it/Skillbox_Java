c040ddf54b301f7b7987c8234c20cbdea2757060
Alexander Gagarin <sacred@xaker.ru>
2021-01-15 16:31:42 +0500

    refactoring method transfer

--- 11_Multithreading/Transactions/src/main/java/Bank.java
+++ 11_Multithreading/Transactions/src/main/java/Bank.java
@@ -1,58 +1,57 @@
 import java.util.HashMap;
 import java.util.Random;
 
-public class Bank
-{
+public class Bank {
 
-    private HashMap<String, Account> accounts;
-    private final Random random = new Random();
+  private HashMap<String, Account> accounts;
+  private final Random random = new Random();
 
-    public HashMap<String, Account> getAccounts() {
-        return accounts;
-    }
+  public HashMap<String, Account> getAccounts() {
+    return accounts;
+  }
 
-    public void setAccounts(HashMap<String, Account> accounts) {
-        this.accounts = accounts;
-    }
+  public void setAccounts(HashMap<String, Account> accounts) {
+    this.accounts = accounts;
+  }
 
-    public synchronized boolean isFraud(String fromAccountNum, String toAccountNum, long amount)
-        throws InterruptedException
-    {
-        Thread.sleep(1000);
-        return random.nextBoolean();
-    }
+  public synchronized boolean isFraud(String fromAccountNum, String toAccountNum, long amount)
+      throws InterruptedException {
+    Thread.sleep(1000);
+    return random.nextBoolean();
+  }
 
-    public void transfer(String fromAccountNum, String toAccountNum, long amount)
-    {
-        synchronized (this) {
-                    if(accounts.get(fromAccountNum).getMoney() - amount > 0) {
-                        if (accounts.get(fromAccountNum).isWithoutBlockAcc() && accounts.get(toAccountNum)
-                        .isWithoutBlockAcc()) {
-                        accounts.get(fromAccountNum).setMoney(accounts.get(fromAccountNum).getMoney() - amount);
-                        accounts.get(toAccountNum).setMoney(accounts.get(toAccountNum).getMoney() + amount);
+  public void transfer(String fromAccountNum, String toAccountNum, long amount) {
+    int fromHash = System.identityHashCode(fromAccountNum);
+    int toHash = System.identityHashCode(toAccountNum);
+    synchronized (fromHash < toHash ? fromAccountNum : toAccountNum) {
+      synchronized (fromHash < toHash ? toAccountNum : fromAccountNum) {
+        if (accounts.get(fromAccountNum).getMoney() - amount > 0) {
+          if (accounts.get(fromAccountNum).isWithoutBlockAcc() && accounts.get(toAccountNum)
+              .isWithoutBlockAcc()) {
+            accounts.get(fromAccountNum).setMoney(accounts.get(fromAccountNum).getMoney() - amount);
+            accounts.get(toAccountNum).setMoney(accounts.get(toAccountNum).getMoney() + amount);
 
-                        if (amount > 50000) {
-                            try {
-                                if (isFraud(fromAccountNum, toAccountNum, amount)) {
-                                    accounts.get(fromAccountNum).setWithoutBlockAcc(false);
-                                    accounts.get(toAccountNum).setWithoutBlockAcc(false);
-                                }
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    } else {
-                        System.out.println("Один или оба счёта заблокированны");
-                    }
-                } else {
-                        System.out.println("На счёте списания недостаточно средств");
-                    }
+            if (amount > 50000) {
+              try {
+                if (isFraud(fromAccountNum, toAccountNum, amount)) {
+                  accounts.get(fromAccountNum).setWithoutBlockAcc(false);
+                  accounts.get(toAccountNum).setWithoutBlockAcc(false);
+                }
+              } catch (InterruptedException e) {
+                e.printStackTrace();
+              }
+            }
+          } else {
+            System.out.println("Один или оба счёта заблокированны");
+          }
+        } else {
+          System.out.println("На счёте списания недостаточно средств");
         }
+      }
     }
+  }
 
-    public long getBalance(String accountNum)
-    {
-        return accounts.get(accountNum).getMoney();
-    }
-
+  public long getBalance(String accountNum) {
+    return accounts.get(accountNum).getMoney();
+  }
 }
--- 11_Multithreading/Transactions/src/main/java/Main.java
+++ 11_Multithreading/Transactions/src/main/java/Main.java
@@ -1,30 +1,19 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
 
 public class Main {
 
   public static void main(String[] args) throws InterruptedException {
     Bank bank = new Bank();
     HashMap<String, Account> accounts = new HashMap<>();
-    accounts.put("1", new Account("1", 11000));
-    accounts.put("2", new Account("2", 220000));
-    accounts.put("3", new Account("3", 330000));
-    accounts.put("4", new Account("4", 440000));
-    accounts.put("5", new Account("5", 150000));
-    accounts.put("6", new Account("6", 260000));
-    accounts.put("7", new Account("7", 370000));
-    accounts.put("8", new Account("8", 480000));
-    accounts.put("9", new Account("9", 190000));
-    accounts.put("10", new Account("10", 150000));
-    accounts.put("11", new Account("11", 250000));
-    accounts.put("12", new Account("12", 350000));
-    accounts.put("13", new Account("13", 450000));
-    accounts.put("14", new Account("14", 80000));
-    accounts.put("15", new Account("15", 10000));
-    accounts.put("16", new Account("16", 40000));
-    accounts.put("17", new Account("17", 155444));
-    accounts.put("18", new Account("18", 95123));
-    accounts.put("19", new Account("19", 752000));
-    accounts.put("20", new Account("20", 5000));
+    List<Thread> threadList = new ArrayList<>();
+    Random random = new Random();
+
+    for (int i = 0; i < 1000; i++) {
+      accounts.put("" + i, new Account("" + i, (int) ((Math.random() * 150000) + Math.random() * 250000)));
+    }
     bank.setAccounts(accounts);
 
     System.out.println(
@@ -34,351 +23,32 @@ public class Main {
     bank.getAccounts()
         .forEach((k, v) -> System.out.printf("\t\tБаланс счёта №%s: %d руб.\n", v.getAccNumber(), v.getMoney()));
 
-    Thread oneThead = new Thread(() -> {
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-      bank.transfer("1", "2", 40000);
-      System.out.println("Баланс счёта №1: " + bank.getBalance("1"));
-      System.out.println("Баланс счёта №2: " + bank.getBalance("2"));
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    },"ONE THEAD");
+    for (int i = 0; i < 1000; i++) {
+      String fromAccNum = "" + random.nextInt(accounts.size());
+      String toAccNum = "" + random.nextInt(accounts.size());
+      Thread thread = new Thread(() -> {
+        try {
+          Thread.sleep(5000);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+        bank.transfer(fromAccNum, toAccNum, 40000 + (int)Math.random() * 20000);
+        try {
+          Thread.sleep(5000);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }, i +" THEAD");
+      thread.start();
+      threadList.add(thread);
+    }
 
-    Thread twoThead = new Thread(() -> {
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-      bank.transfer("2", "1", 150000);
-      System.out.println("Баланс счёта №1: " + bank.getBalance("1"));
-      System.out.println("Баланс счёта №2: " + bank.getBalance("2"));
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    },"TWO THEAD");
+    for (Thread thread : threadList) {
+      thread.join();
+    }
 
-    Thread threeThead = new Thread(() -> {
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-      bank.transfer("5", "6", 50000);
-      System.out.println("Баланс счёта №5: " + bank.getBalance("5"));
-      System.out.println("Баланс счёта №6: " + bank.getBalance("6"));
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    },"THREE THEAD");
-
-    Thread fourThead = new Thread(() -> {
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-      bank.transfer("10", "20", 150000);
-      System.out.println("Баланс счёта №10: " + bank.getBalance("10"));
-      System.out.println("Баланс счёта №20: " + bank.getBalance("20"));
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    },"FOUR THEAD");
-
-    Thread fiveThead = new Thread(() -> {
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-      bank.transfer("10", "20", 150000);
-      System.out.println("Баланс счёта №10: " + bank.getBalance("10"));
-      System.out.println("Баланс счёта №20: " + bank.getBalance("20"));
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    },"FIVE THEAD");
-
-    Thread sixThead = new Thread(() -> {
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-      bank.transfer("10", "20", 150000);
-      System.out.println("Баланс счёта №10: " + bank.getBalance("10"));
-      System.out.println("Баланс счёта №20: " + bank.getBalance("20"));
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    },"SIX THEAD");
-
-    Thread sevenThead = new Thread(() -> {
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-      bank.transfer("10", "20", 150000);
-      System.out.println("Баланс счёта №10: " + bank.getBalance("10"));
-      System.out.println("Баланс счёта №20: " + bank.getBalance("20"));
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    },"SEVEN THEAD");
-
-    Thread eightThead = new Thread(() -> {
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-      bank.transfer("3", "2", 20000);
-      System.out.println("Баланс счёта №3: " + bank.getBalance("3"));
-      System.out.println("Баланс счёта №2: " + bank.getBalance("2"));
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    },"EIGHT THEAD");
-
-    Thread nineThead = new Thread(() -> {
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-      bank.transfer("3", "1", 100000);
-      System.out.println("Баланс счёта №3: " + bank.getBalance("3"));
-      System.out.println("Баланс счёта №1: " + bank.getBalance("1"));
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    },"NINE THEAD");
-
-    Thread tenThead = new Thread(() -> {
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-      bank.transfer("20", "3", 200000);
-      System.out.println("Баланс счёта №3: " + bank.getBalance("3"));
-      System.out.println("Баланс счёта №20: " + bank.getBalance("20"));
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    },"TEN THEAD");
-
-    Thread elevenThead = new Thread(() -> {
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-      bank.transfer("5", "6", 45000);
-      System.out.println("Баланс счёта №5: " + bank.getBalance("5"));
-      System.out.println("Баланс счёта №6: " + bank.getBalance("6"));
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    },"ELEVEN THEAD");
-
-    Thread twelveThead = new Thread(() -> {
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-      bank.transfer("6", "7", 150000);
-      System.out.println("Баланс счёта №6: " + bank.getBalance("6"));
-      System.out.println("Баланс счёта №7: " + bank.getBalance("7"));
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    },"TWELVE THEAD");
-
-    Thread thirteenThead = new Thread(() -> {
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-      bank.transfer("7", "8", 50000);
-      System.out.println("Баланс счёта №7: " + bank.getBalance("7"));
-      System.out.println("Баланс счёта №8: " + bank.getBalance("8"));
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    },"THIRTEEN THEAD");
-
-    Thread fourteenThead = new Thread(() -> {
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-      bank.transfer("8", "11", 150000);
-      System.out.println("Баланс счёта №8: " + bank.getBalance("8"));
-      System.out.println("Баланс счёта №11: " + bank.getBalance("11"));
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    },"FOURTEEN THEAD");
-
-    Thread fiveteenThead = new Thread(() -> {
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-      bank.transfer("11", "7", 150000);
-      System.out.println("Баланс счёта №11: " + bank.getBalance("11"));
-      System.out.println("Баланс счёта №7: " + bank.getBalance("7"));
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    },"FIVETEEN THEAD");
-
-    Thread sixteenThead = new Thread(() -> {
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-      bank.transfer("11", "12", 150000);
-      System.out.println("Баланс счёта №11: " + bank.getBalance("11"));
-      System.out.println("Баланс счёта №12: " + bank.getBalance("12"));
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    },"SIXTEEN THEAD");
-
-    Thread seventeenThead = new Thread(() -> {
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-      bank.transfer("12", "15", 100000);
-      System.out.println("Баланс счёта №12: " + bank.getBalance("12"));
-      System.out.println("Баланс счёта №15: " + bank.getBalance("15"));
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    },"SEVENTEEN THEAD");
-
-    Thread eightteenThead = new Thread(() -> {
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-      bank.transfer("19", "11", 200000);
-      System.out.println("Баланс счёта №19: " + bank.getBalance("19"));
-      System.out.println("Баланс счёта №11: " + bank.getBalance("11"));
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    },"EIGHTTEEN THEAD");
-
-    Thread nineteenThead = new Thread(() -> {
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-      bank.transfer("19", "18", 200000);
-      System.out.println("Баланс счёта №19: " + bank.getBalance("19"));
-      System.out.println("Баланс счёта №18: " + bank.getBalance("18"));
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    },"NINETEEN THEAD");
-
-    Thread twentyThead = new Thread(() -> {
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-      bank.transfer("19", "1", 200000);
-      System.out.println("Баланс счёта №19: " + bank.getBalance("19"));
-      System.out.println("Баланс счёта №1: " + bank.getBalance("1"));
-      try {
-        Thread.sleep(5000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
-    },"TWENTY THEAD");
-
-    oneThead.start();
-    twoThead.start();
-    threeThead.start();
-    fourThead.start();
-    fiveThead.start();
-    sixThead.start();
-    sevenThead.start();
-    eightThead.start();
-    nineThead.start();
-    tenThead.start();
-    elevenThead.start();
-    twelveThead.start();
-    thirteenThead.start();
-    fourteenThead.start();
-    fiveteenThead.start();
-    sixteenThead.start();
-    seventeenThead.start();
-    eightteenThead.start();
-    nineteenThead.start();
-    twentyThead.start();
-
-    twentyThead.join();
     System.out.println(
         "Сумма на счётах в банке: " + bank.getAccounts().values().stream().mapToLong(Account::getMoney).sum()
             + " руб.");
   }
-
 }
