36d93aa4498c2edc410ad6416961b00f6f473178
Alexander Gagarin <sacred@xaker.ru>
2021-08-06 12:10:49 +0500

    actuator dependency added

--- 12_SpringBootWebDevelopment/ToDoListSB/src/main/java/zone/gagarin/todolist/controller/RESTController.java
+++ 12_SpringBootWebDevelopment/ToDoListSB/src/main/java/zone/gagarin/todolist/controller/RESTController.java
@@ -4,14 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import zone.gagarin.todolist.entity.Task;
 import zone.gagarin.todolist.service.TaskService;
 
@@ -53,6 +46,7 @@ public class RESTController {
     if(taskUpdate == null){
       return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
     }
+    taskUpdate.setId(id);
     taskUpdate.setTitle(task.getTitle());
     taskUpdate.setDescription(task.getDescription());
     taskUpdate.setCompleted(task.isCompleted());
@@ -67,8 +61,9 @@ public class RESTController {
   }
 
   @DeleteMapping("/tasks/{id}")
-  public void deleteTaskById(@PathVariable Long id) {
+  public String deleteTaskById(@PathVariable Long id) {
     taskService.deleteById(id);
+    return "Deleting Task by ID = " + id;
   }
 
   @DeleteMapping("/tasks")
