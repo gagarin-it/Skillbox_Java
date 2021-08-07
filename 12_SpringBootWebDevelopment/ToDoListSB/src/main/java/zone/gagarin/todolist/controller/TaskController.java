ff79ba5857d9551e25b86c60e9ad7f364d55c788
Alexander Gagarin <sacred@xaker.ru>
2021-08-07 12:06:27 +0500

    changing CRUD TaskController methods using responseentity

---
+++ 12_SpringBootWebDevelopment/ToDoListSB/src/main/java/zone/gagarin/todolist/controller/TaskController.java
@@ -0,0 +1,99 @@
+package zone.gagarin.todolist.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import zone.gagarin.todolist.entity.Task;
+import zone.gagarin.todolist.service.TaskService;
+
+@RestController
+@RequestMapping("/todolist")
+public class TaskController {
+
+  private final TaskService taskService;
+
+  @Autowired
+  public TaskController(TaskService taskService) {
+    this.taskService = taskService;
+  }
+
+  @GetMapping("/tasks")
+  public ResponseEntity<List<Task>> getAllTasks() {
+    List<Task> tasks = taskService.findAll();
+    if (tasks.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    }
+    return ResponseEntity.status(HttpStatus.OK).body(tasks);
+  }
+
+  @GetMapping("/tasks/{id}")
+  public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
+    Task task = taskService.findById(id);
+    if (task == null) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    }
+    return ResponseEntity.status(HttpStatus.OK).body(task);
+  }
+
+  @PostMapping("/tasks")
+  public ResponseEntity<Task> addNewTask(@RequestBody Task task) {
+    if (task == null) {
+      return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+    } else {
+      taskService.save(task);
+      return ResponseEntity.status(HttpStatus.CREATED).body(task);
+    }
+  }
+
+  @PostMapping("/tasks/{id}")
+  public ResponseEntity<Object> addNewSubtask(@PathVariable String id) {
+    return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(null);
+  }
+
+  @PutMapping("/tasks/{id}")
+  public ResponseEntity<Task> updateTask(@RequestBody Task task, @PathVariable Long id) {
+    Task taskUpdate = taskService.findById(id);
+    if (taskUpdate == null) {
+      taskService.save(task);
+      return ResponseEntity.status(HttpStatus.CREATED).body(task);
+    } else {
+      taskUpdate.setId(id);
+      taskUpdate.setTitle(task.getTitle());
+      taskUpdate.setDescription(task.getDescription());
+      taskUpdate.setCompleted(task.isCompleted());
+      taskUpdate.setDateAndTimeOfCreation(task.getDateAndTimeOfCreation());
+      taskService.save(taskUpdate);
+      return ResponseEntity.status(HttpStatus.OK).body(taskUpdate);
+    }
+  }
+
+  @PutMapping("/tasks")
+  public ResponseEntity<Object> updateAllTasks() {
+    return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(null);
+  }
+
+  @DeleteMapping("/tasks/{id}")
+  public ResponseEntity<String> deleteTaskById(@PathVariable Long id) {
+    Task task = taskService.findById(id);
+    taskService.deleteById(id);
+    return task == null ? ResponseEntity.status(HttpStatus.NOT_FOUND).body(null)
+        : ResponseEntity.status(HttpStatus.OK).body("Deleting Task by ID = " + id);
+}
+
+  @DeleteMapping("/tasks")
+  public ResponseEntity<String> deleteAllTasks() {
+    List<Task> tasks = taskService.findAll();
+    tasks.forEach(t -> taskService.deleteById(t.getId()));
+    return tasks.isEmpty() ? ResponseEntity.status(HttpStatus.NOT_FOUND).body(null)
+        : ResponseEntity.status(HttpStatus.OK).body("Deleting All Tasks");
+  }
+}
