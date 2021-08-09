a60fc005286bbae06b6f119f453a13d3bca29766
Alexander Gagarin <sacred@xaker.ru>
2021-08-09 19:39:30 +0500

    added List subtask, connections @OneToMany and @ManyToOne in Task.class

--- 12_SpringBootWebDevelopment/ToDoListSB/src/main/java/zone/gagarin/todolist/entity/Task.java
+++ 12_SpringBootWebDevelopment/ToDoListSB/src/main/java/zone/gagarin/todolist/entity/Task.java
@@ -2,11 +2,18 @@ package zone.gagarin.todolist.entity;
 
 import com.sun.istack.NotNull;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import org.springframework.hateoas.RepresentationModel;
 
@@ -33,6 +40,22 @@ public class Task extends RepresentationModel<Task> {
   @Column(name = "completed")
   private boolean isCompleted;
 
+  @ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+  @JoinColumn(name = "parent_task_id",referencedColumnName = "id")
+  private Task taskParent;
+
+  @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "taskParent")
+  private List<Task> subtasks;
+
+  public void addSubtask(Task task) {
+    task.setTaskParent(this);
+    subtasks.add(task);
+  }
+
+  public void deleteSubtask(Task task) {
+    subtasks.remove(task);
+  }
+
   public Task() {
     this.dateAndTimeOfCreation = LocalDateTime.now();
   }
@@ -41,6 +64,7 @@ public class Task extends RepresentationModel<Task> {
     this.title = title;
     this.description = description;
     this.dateAndTimeOfCreation = LocalDateTime.now();
+    this.subtasks = new CopyOnWriteArrayList<>();
   }
 
   public Long getId() {
@@ -81,6 +105,22 @@ public class Task extends RepresentationModel<Task> {
 
   public void setCompleted(boolean completed) {
     isCompleted = completed;
+  }
+
+  public List<Task> getSubtasks() {
+    return subtasks;
+  }
+
+  public void setSubtasks(List<Task> subtasks) {
+    this.subtasks = subtasks;
+  }
+
+  public Task getTaskParent() {
+    return taskParent;
+  }
+
+  public void setTaskParent(Task taskParent) {
+    this.taskParent = taskParent;
   }
 
   @Override
